f3a56d3cb9ba24392375d78ea76cbe1e36ee4a20
Cooljackup <cooljackup139@gmail.com>
2026-02-19 12:45:01 -0500

    Exercise 3 completed.

--- Nicholas_COMP254Lab3/src/exercise3/RecursiveFinder.java
+++ Nicholas_COMP254Lab3/src/exercise3/RecursiveFinder.java
@@ -3,6 +3,49 @@
 
 package exercise3;
 
+// Import necessary packages.
+import java.io.File;
+
 public class RecursiveFinder {
 
+    // Method to find files with a matching name provided by the user.
+    public static int fileFinder(String filePath, String fileName) {
+
+        // Instance variables.
+        File path = new File(filePath);
+        File[] files = path.listFiles();
+        String currentFileName;
+        String rawFileName;
+        int fileCount = 0;
+
+        // Checks to see if file path exists provided by the user. If it doesn't, it fails. Just returns as no files.
+        if (!path.exists()) {
+            return 0;
+        }
+
+        // If the directory exists, it proceeds and starts to run through each file.
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    currentFileName = file.getName();
+
+                    // When checking, it removes the file type extension and then compares the file name with what was requested. If it matches, it counts up one.
+                    int fileType = currentFileName.lastIndexOf('.');
+                    rawFileName = (fileType == -1) ? currentFileName : currentFileName.substring(0, fileType);
+                    if (rawFileName.equalsIgnoreCase(fileName)) {
+                        System.out.println("Located file at: " + file.getAbsolutePath());
+                        fileCount++;
+                    }
+                }
+
+                // If it is a directory, it recurses in it.
+                if (file.isDirectory()) {
+                    fileCount += fileFinder(file.getAbsolutePath(), fileName);
+                }
+            }
+        }
+
+        // Returns how many files were located.
+        return fileCount;
+    }
 }
--- Nicholas_COMP254Lab3/src/exercise3/RecursiveFinderDriver.java
+++ Nicholas_COMP254Lab3/src/exercise3/RecursiveFinderDriver.java
@@ -3,8 +3,30 @@
 
 package exercise3;
 
+// Import necessary packages.
+import java.util.Scanner;
+
 public class RecursiveFinderDriver {
     public static void main(String[] args) {
 
+        // Instance variables.
+        String filePath;
+        String fileName;
+        Scanner scanner = new Scanner(System.in);
+        int fileCount;
+
+        // Prompts the user to enter the directory path they want to search, then prompts the user for the file name to search for it.
+        System.out.print("Please enter the directory path you want to search in: ");
+        filePath = scanner.nextLine();
+        System.out.print("Please enter the name of the file you want to search for: ");
+        fileName = scanner.nextLine();
+
+        // Calls the recursive method and searches every file for what the user inputted, then prints the result.
+        fileCount = RecursiveFinder.fileFinder(filePath, fileName);
+        if (fileCount != 0) {
+            System.out.println("\nThere was " + fileCount + " file(s) with the name '" + fileName + "' found.");
+        } else {
+            System.out.println("\nERROR. There was no files with the name '" + fileName + "' found.");
+        }
     }
 }
