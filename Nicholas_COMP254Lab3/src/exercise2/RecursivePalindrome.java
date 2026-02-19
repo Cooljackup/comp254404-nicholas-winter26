f840965551e71554ef45d4d8c6930a96368953e9
Cooljackup <cooljackup139@gmail.com>
2026-02-18 20:28:41 -0500

    Exercise 2 completed.

--- Nicholas_COMP254Lab3/src/exercise2/RecursivePalindrome.java
+++ Nicholas_COMP254Lab3/src/exercise2/RecursivePalindrome.java
@@ -5,4 +5,23 @@ package exercise2;
 
 public class RecursivePalindrome {
 
+    // Method to check the word given by the user is a palindrome.
+    public static boolean checkPalindrome(String input) {
+
+        // Makes the word provided by the user all lowercase so if the user capitalizes any letter it won't fail the check.
+        input = input.toLowerCase();
+
+        // Checks to make sure if there is 1 or 0 letters left, it returns true. Used for odd number words. Also prevents errors for 1 or 0 characters.
+        if (input.length() <= 1) {
+            return true;
+        }
+
+        // Checks to see if the first and last letters match, if it doesn't, returns false.
+        if (input.charAt(0) != input.charAt(input.length() - 1)) {
+            return false;
+        }
+
+        // Removes the first and last letters and then repeats method to check the middle letters until there is no more letters.
+        return checkPalindrome(input.substring(1, input.length() - 1));
+    }
 }
--- Nicholas_COMP254Lab3/src/exercise2/RecursivePalindromeDriver.java
+++ Nicholas_COMP254Lab3/src/exercise2/RecursivePalindromeDriver.java
@@ -3,8 +3,27 @@
 
 package exercise2;
 
+// Import necessary packages.
+import java.util.Scanner;
+
 public class RecursivePalindromeDriver {
     public static void main(String[] args) {
 
+        // Instance variables.
+        String input;
+        Scanner scanner = new Scanner(System.in);
+        boolean palindromeCheck;
+
+        // Prompts the user to enter a word, then stores it into the scanner.
+        System.out.print("Please enter a word: ");
+        input = scanner.nextLine();
+
+        // Calls the recursive method to check to see if it's a palindrome, then prints the result.
+        palindromeCheck = RecursivePalindrome.checkPalindrome(input);
+        if (palindromeCheck) {
+            System.out.println("\nThe word '" + input + "' is a palindrome.");
+        } else {
+            System.out.println("\nThe word '" + input + "' is not a palindrome.");
+        }
     }
 }
