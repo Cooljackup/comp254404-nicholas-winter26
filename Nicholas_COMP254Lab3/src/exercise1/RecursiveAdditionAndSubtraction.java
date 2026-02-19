d87fb588e80ad58ea402552dcd9983235a2202e0
Cooljackup <cooljackup139@gmail.com>
2026-02-18 19:40:51 -0500

    Exercise 1 completed.

--- Nicholas_COMP254Lab3/src/exercise1/RecursiveAdditionAndSubtraction.java
+++ Nicholas_COMP254Lab3/src/exercise1/RecursiveAdditionAndSubtraction.java
@@ -5,4 +5,15 @@ package exercise1;
 
 public class RecursiveAdditionAndSubtraction {
 
+    // Method to calculate the product of m and n, which are provided by the user.
+    public static int calculation(int m, int n) {
+
+        // Checks to make sure either integer is not 0, so it doesn't crash.
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+
+        // Returns the product of m and n.
+        return calculation(m - 1, n) + n;
+    }
 }
--- Nicholas_COMP254Lab3/src/exercise1/RecursiveAdditionAndSubtractionDriver.java
+++ Nicholas_COMP254Lab3/src/exercise1/RecursiveAdditionAndSubtractionDriver.java
@@ -3,8 +3,26 @@
 
 package exercise1;
 
+// Import necessary packages.
+import java.util.Scanner;
+
 public class RecursiveAdditionAndSubtractionDriver {
     public static void main(String[] args) {
 
+        // Instance variables.
+        int m;
+        int n;
+        Scanner scanner =  new Scanner(System.in);
+        int calculationResult;
+
+        // Prompts the user to input an integer for both m and n, then stores into the scanner.
+        System.out.print("Please enter an integer: ");
+        m = scanner.nextInt();
+        System.out.print("Please enter a second integer: ");
+        n = scanner.nextInt();
+
+        // Once both integers have been given, calculates the product of m and n, then prints it.
+        calculationResult = RecursiveAdditionAndSubtraction.calculation(m, n);
+        System.out.println("\nThe product of " + m + " and " + n + " is " + calculationResult + ".");
     }
 }
